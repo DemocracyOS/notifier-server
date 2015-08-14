fcb70c39b5e6da47445b9d1855dde8a9db66dfe8
Matías Lescano <matiasj.lescano@gmail.com>
2015-08-14 12:12:24 -0300

    fix ssl redirection, only when protocol is http

--- server/middlewares/ssl.js
+++ server/middlewares/ssl.js
@@ -24,8 +24,7 @@ const prod = env === 'production'
 //TODO: replace this with `ssl` module from `DemocracyOS/app`
 
 export default function herokuSslRedirect(req, res, next) {
-  log('x-forwarded-proto', req.headers['x-forwarded-proto'])
-  const https = req.headers['x-forwarded-proto'] != 'https'
+  const https = req.headers['x-forwarded-proto'] === 'https'
   if (!prod || https) return next()
 
   const redirectionUrl = `https://${req.host}${req.originalUrl}`
